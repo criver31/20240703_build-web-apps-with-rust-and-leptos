3aa67d0fee738b8f6b3cc74ab131f909aa639e19
fine <54142345+criver31@users.noreply.github.com>
2024-07-30 02:38:49 +0000

    add button and move keyword

--- projects/tools-app/src/components/parent_child/parent_child_write_signal.rs
+++ projects/tools-app/src/components/parent_child/parent_child_write_signal.rs
@@ -4,8 +4,22 @@ use leptos::*;
 pub fn Parent() -> impl IntoView {
     let (counter, set_counter) = create_signal(0);
 
-    let increment = |_| set_counter.update(|c| *c += 1);
-    let decrement = |_| set_counter.update(|c| *c -= 1);
+    let increment_counter = move |_| set_counter.update(|c| *c += 1);
+    let decrement_counter = move |_| set_counter.update(|c| *c -= 1);
 
-    view! { <p>"Parent Child Write Signal."</p> }
+    view! {
+        <div>
+            <div style="border: 1px solid black; margin: 4px"></div>
+            <h3>"Parent Child Write Signal."</h3>
+            <p>"Counter: " {counter}</p>
+            <div>
+                <button type="button" on:click=increment_counter>
+                    "Parent + 1"
+                </button>
+                <button type="button" on:click=decrement_counter>
+                    "Parent - 1"
+                </button>
+            </div>
+        </div>
+    }
 }
